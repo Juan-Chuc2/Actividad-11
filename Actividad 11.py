9a14f497d6bf751a3e04e88b03daa4af2c4f91e5
Juan Diego <chucjuan508@gmail.com>
2025-07-31 09:15:52 -0800

    dicionarios y inputs

---
+++ Actividad 11.py
@@ -0,0 +1,17 @@
+propetarios ={}
+vehiculos ={}
+print("\n Pago de impuesto vehicular")
+cant_user = int(input("Cuantos usuarios desesea ingresar: "))
+for i in range(cant_user):
+    print(f"\n Propetiario #{i+1}")
+    number_nit = int(input("Ingrese su numero de NIT: "))
+    complet_name = input("Ingrese su nombre completo porfavor: ")
+    number_contact = int(input("Ingrese su número de telefono: "))
+    cantidad_vehiculos = int(input("Ingrese la cantidad de vehiculos que posee: "))
+    for j in range(cantidad_vehiculos):
+        print(f"\n Vehivulo {j+1}")
+        placa = input("Ingrese el numero de placa: ")
+        marca = input("Ingrese la marca de su vehiculo: ")
+        modelo = int(input("Ingrese el modelo de su vehiculo (ej: corrolla): "))
+        anio = int(input("Ingrese el año de su vehiculo: "))
+        estado_impuesto = input("Ingrese si a pagado su impuesto (si/no): ")
